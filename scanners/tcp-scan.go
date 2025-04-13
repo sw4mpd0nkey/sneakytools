6d75609754299428682d4181f291f069338c2c7d
sw4mpd0nkey <sw4mpd0nkey@protonmail.com>
2025-04-12 21:59:48 -0400

    fixed non-concurrent version

--- scanners/tcp-scan.go
+++ scanners/tcp-scan.go
@@ -3,6 +3,7 @@ package scanners
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type TcpScanner struct {
@@ -21,16 +22,22 @@ func NewTcpScanner(u string, p uint) *TcpScanner {
 
 func (t TcpScanner) TcpScan() {
 
+	var wg sync.WaitGroup
 	fmt.Printf("Scanning %v...\n\n", t.url)
 
 	for i := 1; i <= int(t.ports); i++ {
-		address := fmt.Sprintf(t.url+":%d", i)
-		conn, err := net.Dial(t.protocol, address)
-		if err != nil {
-			// port is closed or filtered
-			continue
-		}
-		conn.Close()
-		fmt.Printf("%d open\n", i)
+		wg.Add(1)
+		go func(j int) {
+			defer wg.Done()
+			address := fmt.Sprintf(t.url+":%d", j)
+			conn, err := net.Dial(t.protocol, address)
+			if err != nil {
+				// port is closed or filtered
+				return
+			}
+			conn.Close()
+			fmt.Printf("%d open\n", j)
+		}(i)
 	}
+	wg.Wait()
 }
